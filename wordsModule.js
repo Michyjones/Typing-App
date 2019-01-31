e9df11b2d444b660288c2c58d5e6f964a476200c
Michyjones <mykembuguah11@gmail.com>
2019-01-31 14:55:10 +0300

    Add the word module for the application

---
+++ wordsModule.js
@@ -0,0 +1,14 @@
+var wordModule = (function(){
+    var words = [
+        "word1 w2 w3 w4 w5 w6",
+        "word1 w2 w3 w4 w5 w6",
+        "word1 w2 w3 w4 w5 w6",
+        "word1 w2 w3 w4 w5 w6"
+    ];
+    return {
+        getWords(textNumber){
+        return words [textNumber];
+
+        }
+    }
+})();
