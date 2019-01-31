06e1feaf9a7917c74b83bbedf11cf5ce5bfa75db
Michyjones <mykembuguah11@gmail.com>
2019-01-31 14:54:18 +0300

    Add event handler module

---
+++ eventModule.js
@@ -0,0 +1,10 @@
+var eventModule = (function(dataModule, UIModule, wordsModule, certificateModule){
+    var addEventListeners = function(){
+
+    };
+    return{
+        init: function(duration, textNumber){
+            addEventListeners();
+        }
+    };
+})(dataModule, UIModule, wordsModule, certificateModule);
