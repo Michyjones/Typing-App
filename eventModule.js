4757220247bc36ec34ad2b370d744505de34ae60
Michyjones <mykembuguah11@gmail.com>
2019-02-01 17:14:45 +0300

    Update the event module by adding set, intializa and update time

--- eventModule.js
+++ eventModule.js
@@ -1,9 +1,19 @@
-var eventModule = (function(dataModule, UIModule, wordsModule, certificateModule){
+var eventModule = (function(dModule, uModule, wModule, cModule){
     var addEventListeners = function(){
 
     };
     return{
         init: function(duration, textNumber){
+            var words = wModule.getWords(textNumber);
+            dModule.fillListOfTestWords(textNumber, words);
+
+            var returnLine = dModule.getReturnLine();
+            var testWords = dModule.getListOfTestWords();
+            uModule.fillContent(testWords, returnLine);
+            dModule.setTestTime(duration);
+            dModule.IntialTimeLeft();
+            var timeLeft = dModule.getTimeleft();
+            uModule.updateTimeLeft(timeLeft);
             addEventListeners();
         }
     };
