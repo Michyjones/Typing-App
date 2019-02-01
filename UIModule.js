f66c259e9174855a0ca6d0f56954ee59e72d75df
Michyjones <mykembuguah11@gmail.com>
2019-02-01 17:10:18 +0300

    Update the user Interface by adding more features

--- UIModule.js
+++ UIModule.js
@@ -1,37 +1,73 @@
- var UIModule = (function(){
+var UIModule = (function() {
+  var splitArray = function(string) {
+    return string.split('');
+  };
+  var addSpace = function(array) {
+    array.push('');
+    return array;
+  };
+  var addSpanTags = function(array) {
+    return array.map(function(currentChar) {
+      return '<span>' + currentChar + '</span>';
+    });
+  };
+  var addWordSpanTags = function(array) {
+    array.push('</span>');
+    array.unshift('<span>');
+    return array;
+  };
 
-    var DOMElement ={
-        timeLeft,
-        wpm,
-        wpmChange,
-        cpm,
-        cpmChange,
-        accurancy,
-        accurancyChange,
-        textInput,
-        nameInput,
-        content,
-        activeWord,
-        modal
-    };
-    return{
-        getDOMElement: function(){},
-        updateTimeLeft: function(){},
-        updateResults: function(){},
-        fillModal: function(){},
-        showModal: function(){},
-        inputFocus: function(){},
-        isNameEmpty:function(){},
-        flagNameInput: function(){},
-        spacePressed: function(){},
-        enterPressed: function(){},
-        emptyInput: function(){},
-        getTypedWord: function(){},
-        fillContent: function(){},
-        formatWord: function(wordObject, wordHTML){},
-        setActiveWord: function(index){},
-        deactivateCurrentWord: function(){},
-        scroll: function(){}
+  var joinWords = function(array) {
+    return array.join('');
+  };
 
-    }
- })();
+  var DOMElement = {
+    timeLeft: document.getElementById('timeLeft'),
+    wpm: document.getElementById('wpm'),
+    wpmChange: document.getElementById('wpmChange'),
+    cpm: document.getElementById('cpm'),
+    cpmChange: document.getElementById('cpmChange'),
+    accurancy: document.getElementById('acc'),
+    accurancyChange: document.getElementById('accChange'),
+    textInput: document.querySelector('#input'),
+    nameInput: document.querySelector('.form-group'),
+    content: document.getElementById('content'),
+    activeWord: '',
+    modal: $('#modal')
+  };
+  return {
+    getDOMElement: function() {},
+    updateTimeLeft: function(x) {
+      DOMElement.timeLeft.innerHTML = x;
+    },
+    updateResults: function() {},
+    fillModal: function() {},
+    showModal: function() {},
+    inputFocus: function() {},
+    isNameEmpty: function() {},
+    flagNameInput: function() {},
+    spacePressed: function() {},
+    enterPressed: function() {},
+    emptyInput: function() {},
+    getTypedWord: function() {},
+
+    fillContent: function(array, returnLine) {
+      var content = array.map(splitArray);
+      content = content.map(addSpace);
+      content = content.map(addSpanTags);
+      content = content.map(addWordSpanTags);
+      content = content.map(joinWords);
+      content = content.join(' ');
+      content = content
+        .split('<span>' + returnLine + '</span>')
+        .join('<span>&crarr;</span>');
+      console.log(content);
+
+      DOMElement.content.innerHTML = content;
+    },
+    formatWord: function(wordObject, wordHTML) {},
+    setActiveWord: function(index) {},
+    deactivateCurrentWord: function() {},
+    scroll: function() {}
+  };
+})();
