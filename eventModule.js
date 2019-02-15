eac1563b1cb9311656ef5af012b24b7e10731c9d
Michyjones <mykembuguah11@gmail.com>
2019-02-16 00:47:40 +0300

    [Feature] Add start test, spacepressed  and enter pressed on the event module

--- eventModule.js
+++ eventModule.js
@@ -1,5 +1,20 @@
 var eventModule = (function(dModule, uModule, wModule, cModule) {
   var addEventListeners = function() {};
+  uModule
+    .getDOMElement()
+    .textInput.addEventListener('keydown', function(event) {
+      if (dModule.testEnded()) {
+        return;
+      }
+      var key = event.keyCode;
+      if (key == 13) {
+        uModule.getDOMElement().textInput.value +=
+          dModule.getReturnLine() + ' ';
+        var inputEvent = new Event('input');
+        uModule.getDOMElement().textInput.dispatchEvent(inputEvent);
+      }
+    });
+  window.addEventListener('resize', uModule.scroll);
   return {
     init: function(duration, textNumber) {
       var words = wModule.getWords(textNumber);
@@ -25,12 +40,36 @@ var eventModule = (function(dModule, uModule, wModule, cModule) {
             return;
           }
           if (!dModule.testStarted()) {
+            dModule.StartTest();
+            var counter = setInterval(function() {
+                var results = {};
+                [results.wpm, results.wpmChange] = dModule.calculateWpm();
+                dModule.returnData();
+          if (dModule.timeLeft()){
+              var timeLeft = dModule.reduceTime();
+              uModule.updateTimeLeft(timeLeft); 
+          }
+            }, 1000);
+
           }
           var typedWord = uModule.getTypedWord();
           dModule.updateCureentWord(typedWord);
 
           var currentWord = dModule.getCurrentWord();
           uModule.formatWord(currentWord);
+          if (
+            uModule.spacePressed(event) ||
+            uModule.enterPressed(dModule.getReturnLine())
+          ) {
+            uModule.emptyInput();
+            uModule.deactivateCurrentWord();
+            dModule.moveToNewWord();
+            var index = dModule.getCurrentWordIndex();
+            uModule.setActiveWord(index);
+            var currentWord = dModule.getCurrentWord();
+            uModule.formatWord(currentWord);
+            uModule.scroll();
+          }
         });
       addEventListeners();
     }
